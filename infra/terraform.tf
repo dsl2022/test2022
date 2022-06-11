7289c1a710e64ce0f34b1d2d7d9754bfd49c7804
dsl2055 <job2019tmm@gmail.com>
2022-06-11 18:07:25 -0400

    updated

---
+++ infra/terraform.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "dsl-2022"
+
+    workspaces {
+      name = "gh-multi-channel-message"
+    }
+  }
+}
