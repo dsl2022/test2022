4efe7460537a45a514b65d674e3ce5d517ad29ae
dsl2055 <job2019tmm@gmail.com>
2022-06-11 19:44:43 -0400

    updated

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
+      version = "4.17.1"
     }
     random = {
       source  = "hashicorp/random"
       version = "3.0.1"
     }
   }
-  required_version = ">= 0.12"
+  required_version = ">= 1.2.0"
 
   cloud {
     organization = "dsl-2022"
