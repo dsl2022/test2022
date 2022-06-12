7bd21bba25164024f634e62a54cbafe2b932df47
dsl2055 <job2019tmm@gmail.com>
2022-06-12 18:01:57 -0400

    updated

---
+++ infra/sns.tf
@@ -0,0 +1,31 @@
+locals {
+  emails = ["jobnewsletter2019@gmail.com"]
+}
+
+resource "aws_sns_topic" "topic" {
+  name            = "new-channel-topic"
+  delivery_policy = jsonencode({
+    "http" : {
+      "defaultHealthyRetryPolicy" : {
+        "minDelayTarget" : 20,
+        "maxDelayTarget" : 20,
+        "numRetries" : 3,
+        "numMaxDelayRetries" : 0,
+        "numNoDelayRetries" : 0,
+        "numMinDelayRetries" : 0,
+        "backoffFunction" : "linear"
+      },
+      "disableSubscriptionOverrides" : false,
+      "defaultThrottlePolicy" : {
+        "maxReceivesPerSecond" : 1
+      }
+    }
+  })
+}
+
+resource "aws_sns_topic_subscription" "topic_email_subscription" {
+  count     = length(local.emails)
+  topic_arn = aws_sns_topic.topic.arn
+  protocol  = "email"
+  endpoint  = local.emails[count.index]
+}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
-import { handlerHello } from './hello';
-
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
-export async function handler(event: any, context: any, callback: any) {
-    console.log('event', event.Records);
-    switch (event.resolve) {
-        case 'hello':
-            callback(null, handlerHello());
-            break;
-        default:
-            callback('Error: no resolver found', null);
-    }
+const getData = (data): any =>
+    Object.keys(data).reduce((acc: any, params: any): any => {
+        const { S, N, B } = data[params];
+        if (S) acc[params] = S;
+        if (N) acc[params] = N;
+        if (B) acc[params] = B;
+        return { ...acc };
+    }, {});
+
+export async function handler({ Records }: { Records: any[] }) {
+    try {
+        Records.filter(({ eventName }) => eventName === 'INSERT').forEach(async (record) => {
+            const data = getData(record.dynamodb.NewImage);
+            console.log(data);
+        });
+    } catch (e) {}
+    // switch (event.resolve) {
+    //     case 'hello':
+    //         callback(null, handlerHello());
+    //         break;
+    //     default:
+    //         callback('Error: no resolver found', null);
+    // }
 }
